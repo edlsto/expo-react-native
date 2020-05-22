df0d18b119d7d9e629819bbbf65a399f7adf0029
Edward Stoner <edwardlstoner@gmail.com>
2020-05-22 16:49:07 -0600

    Create ColorPalette component

---
+++ screens/ColorPalette.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { FlatList, StyleSheet, Text } from 'react-native';
+import ColorBox from '../components/ColorBox';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const ColorPalette = ({ route }) => {
+  console.log(route.params.colors);
+  const { palette, paletteName } = route.params;
+  return (
+    <SafeAreaView>
+      <FlatList
+        style={styles.container}
+        data={palette}
+        keyExtractor={(item) => item.hexCode}
+        renderItem={({ item }) => (
+          <ColorBox colorName={item.colorName} colorHex={item.hexCode} />
+        )}
+        ListHeaderComponent={<Text style={styles.text}>{paletteName}</Text>}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 5,
+    marginHorizontal: 20,
+    marginTop: 50,
+  },
+
+  text: {
+    fontWeight: 'bold',
+  },
+});
+export default ColorPalette;
