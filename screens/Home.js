b02d4abde89014b74a48aa28e25b9f2cce17e9ab
Edward Stoner <edwardlstoner@gmail.com>
2020-05-22 19:29:05 -0600

    Add network request

--- screens/Home.js
+++ screens/Home.js
@@ -1,62 +1,28 @@
-import React from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import { StyleSheet, FlatList } from 'react-native';
 import PalettePreview from '../components/PalettePreview';
 
-const SOLARIZED = [
-  { colorName: 'Base03', hexCode: '#002b36' },
-  { colorName: 'Base02', hexCode: '#073642' },
-  { colorName: 'Base01', hexCode: '#586e75' },
-  { colorName: 'Base00', hexCode: '#657b83' },
-  { colorName: 'Base0', hexCode: '#839496' },
-  { colorName: 'Base1', hexCode: '#93a1a1' },
-  { colorName: 'Base2', hexCode: '#eee8d5' },
-  { colorName: 'Base3', hexCode: '#fdf6e3' },
-  { colorName: 'Yellow', hexCode: '#b58900' },
-  { colorName: 'Orange', hexCode: '#cb4b16' },
-  { colorName: 'Red', hexCode: '#dc322f' },
-  { colorName: 'Magenta', hexCode: '#d33682' },
-  { colorName: 'Violet', hexCode: '#6c71c4' },
-  { colorName: 'Blue', hexCode: '#268bd2' },
-  { colorName: 'Cyan', hexCode: '#2aa198' },
-  { colorName: 'Green', hexCode: '#859900' },
-];
-
-const RAINBOW = [
-  { colorName: 'Red', hexCode: '#FF0000' },
-  { colorName: 'Orange', hexCode: '#FF7F00' },
-  { colorName: 'Yellow', hexCode: '#FFFF00' },
-  { colorName: 'Green', hexCode: '#00FF00' },
-  { colorName: 'Violet', hexCode: '#8B00FF' },
-];
-
-const FRONTEND_MASTERS = [
-  { colorName: 'Red', hexCode: '#c02d28' },
-  { colorName: 'Black', hexCode: '#3e3e3e' },
-  { colorName: 'Grey', hexCode: '#8a8a8a' },
-  { colorName: 'White', hexCode: '#ffffff' },
-  { colorName: 'Orange', hexCode: '#e66225' },
-];
-
-const COLOR_PALETTES = [
-  {
-    paletteName: 'Solarized',
-    palette: SOLARIZED,
-  },
-  {
-    paletteName: 'Rainbow',
-    palette: RAINBOW,
-  },
-  {
-    paletteName: 'Frontend Masters',
-    palette: FRONTEND_MASTERS,
-  },
-];
-
 const Home = ({ navigation }) => {
+  const [colors, setColors] = useState([]);
+  const handleColorFetch = useCallback(async () => {
+    const result = await fetch(
+      'https://color-palette-api.kadikraman.now.sh/palettes',
+    );
+    const fetchedColors = await result.json();
+    console.log(fetchedColors);
+    if (result.ok) {
+      setColors(fetchedColors);
+    }
+  }, []);
+
+  useEffect(() => {
+    handleColorFetch();
+  });
+
   return (
     <FlatList
       style={styles.list}
-      data={COLOR_PALETTES}
+      data={colors}
       keyExtractor={(item) => item.paletteName}
       renderItem={({ item }) => (
         <PalettePreview
